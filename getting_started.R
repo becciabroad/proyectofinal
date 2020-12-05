77be9c7c347cb38071722eaffc85df27491f8b1f
Rebecca Hoffmann <rebecca@becci.dk>
2020-12-05 18:00:04 -0300

    commit installed website

---
+++ getting_started.R
@@ -0,0 +1,10 @@
+#Para empezar a armar la pagina web aprendo la libraría de blogdown
+library(blogdown)
+
+# Genero un nueva site utilizando el diseño que eligó
+blogdown::new_site(theme = 'MarcusVirg/forty')
+
+#Setting up the Global setting of the website
+#New posts will be created as .rmd files and with pre-set author
+file.edit('~/.Rprofile')
+options(blogdown.ext = '.Rmd', blogdown.author = 'Rebecca Hoffmann')
